915daa6249d2f2ad086bf7c0aca6dccd6cc84ee8
Emmanuel Prajeeth Jakaregari <emmanuelprajeeth.jakaregari@nxp.com>
2022-06-09 20:58:48 +0530

    Working on Strings

---
+++ strings/concat.py
@@ -0,0 +1,7 @@
+
+def attach(arg1, arg2):
+    return arg1 + arg2
+arg1, arg2 = input("Enter two strings with comma sperator: ").strip().split(",")
+print(input("Enter two strings with comma sperator: ").strip().split(","))
+arg1, arg2 = int(input("Enter two intergers with comma sperator: ")).strip().split(",")
+print(attach(num1, num2))
